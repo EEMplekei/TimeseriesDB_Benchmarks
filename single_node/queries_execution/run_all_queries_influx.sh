127de07999302c83bd80ac8879428ac2b5734bee
VM_Machine <ubuntu@snf-40284.ok-kno.grnetcloud.net>
2024-01-09 12:54:43 +0000

    ADDED LARGE METRICS OF INFLUX

--- single_node/queries_execution/run_all_queries_influx.sh
+++ single_node/queries_execution/run_all_queries_influx.sh
@@ -5,3 +5,11 @@ bash run_queries_influx.sh single large;
 bash run_queries_influx.sh 10_queries small;
 bash run_queries_influx.sh 10_queries medium;
 bash run_queries_influx.sh 10_queries large;
+
+bash run_queries_influx.sh 100_queries small;
+bash run_queries_influx.sh 100_queries medium;
+bash run_queries_influx.sh 100_queries large;
+
+bash run_queries_influx.sh 1000_queries small;
+bash run_queries_influx.sh 1000_queries medium;
+bash run_queries_influx.sh 1000_queries large;
