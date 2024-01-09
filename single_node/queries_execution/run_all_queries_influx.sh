b82763d78924e76db8c79815953a556211943c64
George Sotiropoulos <gsotiropoulos@msi.gr>
2024-01-09 14:27:56 +0200

    add script to run all queries

---
+++ single_node/queries_execution/run_all_queries_influx.sh
@@ -0,0 +1,7 @@
+bash run_queries_influx.sh single small;
+bash run_queries_influx.sh single medium;
+bash run_queries_influx.sh single large;
+
+bash run_queries_influx.sh 10_queries small;
+bash run_queries_influx.sh 10_queries medium;
+bash run_queries_influx.sh 10_queries large;
