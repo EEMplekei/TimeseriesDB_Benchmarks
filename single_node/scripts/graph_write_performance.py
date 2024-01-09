4bf8618b581c320e4838e09541099dcc42f051dc
Michail Tsilimigkounakis <miketsili7@gmail.com>
2024-01-09 15:40:20 +0200

    py script for graph performance

--- single_node/scripts/graph_write_performance.py
+++ single_node/scripts/graph_write_performance.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-database_names = ["influx"] 
-dataset_sizes = ["small"]
+database_names = ["timescale","influx"] 
+dataset_sizes = ["large"]
 for ds_size in dataset_sizes:
     rows_per_sec = []
     metrics_per_sec = []
     for db_name in database_names:
-        with open(f"/home/ubuntu/TimeseriesDB_Benchmarks/single_node/performance/write/{db_name}_{ds_size}.out") as f:
+        with open(f"../performance/write/{db_name}_{ds_size}.out") as f:
             lines = f.readlines()
             for l in lines:
                 if l[0:6] == 'loaded':
@@ -16,8 +17,8 @@ for ds_size in dataset_sizes:
     fig, (ax1, ax2) = plt.subplots(2,1 ,figsize=(8,6))
     fig.suptitle(f"{ds_size.capitalize()} dataset insert performance comparison")
     plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.5)    
-    ax1.bar(database_names, metrics_per_sec, color=['orange', 'purple'], width = 0.2)
-    ax2.bar(database_names, rows_per_sec, color=['orange', 'purple'], width = 0.2)
+    ax1.bar(database_names, metrics_per_sec, color=['green', 'purple'], width = 0.2)
+    ax2.bar(database_names, rows_per_sec, color=['green', 'purple'], width = 0.2)
     ax1.set_xlabel("Database") , ax2.set_xlabel("Database")
     ax1.set_ylabel("Metrics / second") , ax2.set_ylabel("Rows / second")
     plt.show()            
