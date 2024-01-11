8cfe3a1108ec26fe5dc6699f46eac5df68d6ef39
VM_Machine <ubuntu@snf-40284.ok-kno.grnetcloud.net>
2024-01-11 09:38:01 +0000

    Influx benchmarkings except large 100

--- single_node/benchmarking/timescale_benchmark.sh
+++ single_node/benchmarking/timescale_benchmark.sh
@@ -7,20 +7,21 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 db_sizes=("small" "medium")
-cd ..
 
 ##Small, medium datasets
 echo -e "${YELLOW}### Loading data into databases${NC}"
 
 # Load into database small, medium datasets
+cd ~/TimeseriesDB_Benchmarks/single_node/data_load
 for db in "${db_sizes[@]}"; do
     echo -e "${GREEN}Loading data into InfluxDB for $db dataset${NC}"
-    bash data_load/load_timescale.sh "$db"
+    bash load_timescale.sh "$db"
 done
 
 #Run queries for small, medium datasets
+cd ~/TimeseriesDB_Benchmarks/single_node/queries_execution
 echo -e "${YELLOW}### Running scripts to load data into TimescaleDB for small, medium datasets${NC}"
-bash queries_execution/run_smallmedium_queries_timescale.sh
+bash run_smallmedium_queries_timescale.sh
 
 echo -e "${YELLOW}### Dropping databases${NC}"
 
@@ -36,12 +37,14 @@ exit
 
 ##Large dataset
 # Load into database small, medium datasets
+cd ~/TimeseriesDB_Benchmarks/single_node/data_load
 echo -e "${GREEN}Loading data into InfluxDB for large dataset${NC}"
-bash data_load/load_timescale.sh large
+bash load_timescale.sh large
 
 #Run queries for small, medium datasets
+cd ~/TimeseriesDB_Benchmarks/single_node/queries_execution
 echo -e "${YELLOW}### Running scripts to load data into TimescaleDB for lage dataset${NC}"
-bash queries_execution/run_large_queries_timescale.sh
+bash run_large_queries_timescale.sh
 
 echo -e "${YELLOW}### Dropping databaset${NC}"
 
@@ -55,4 +58,4 @@ exit
 
 echo -e "${YELLOW}### Clearing TimescaleDB cache${NC}"
 # Clear Timescale cache just in case
-bash queries_execution/clear_caches/timescale_clr_cache.sh
+bash clear_caches/timescale_clr_cache.sh
