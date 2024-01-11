17aa0d2485bd17049213f331649fc5adccca1a5b
Panagiotis Stefanis <panstef5555@gmail.com>
2024-01-11 18:10:54 +0200

    added values to plot

--- single_node/scripts/avg_query_plot.py
+++ single_node/scripts/avg_query_plot.py
@@ -3,9 +3,9 @@ import parse_file
 import os
 import matplotlib.pyplot as plt
 
-databases = ["influx","influx"]
-nbr_queries = ["1", "10","100"]
-dataset_size = ["small", "medium","large"]
+databases = ["influx", "timescale"]
+nbr_queries = ["1", "10"]
+dataset_size = ["small", "medium", "large"]
 for nbr_query in nbr_queries:
     # Initialize lists to store means and database names for each query count
     means_list = []
@@ -14,7 +14,8 @@ for nbr_query in nbr_queries:
     for database in databases:
         means_per_database = []
         for size in dataset_size:
-            file_path = os.path.join('..', 'performance', 'queries', database, f'{nbr_query}_queries', f'{database}_{nbr_query}_queries_{size}.out')
+            file_path = os.path.join('..', 'performance', 'queries', f'{database}db' if database == "timescale" else database,
+                                     f'{nbr_query}_queries', f'{database}_{nbr_query}_queries_{size}.out')
             try:
                 means = parse_file.parse_file(file_path)
                 # print("database -" + database+ " , size - " + size + ", nbr_query - " +nbr_query)
@@ -27,17 +28,19 @@ for nbr_query in nbr_queries:
         if means_per_database:
             means_list.append(means_per_database)
             database_names.append(database)
-    
+
     # Create a bar plot for each number of queries
     x = np.arange(len(dataset_size))
     width = 0.2  # Width of the bars
 
     for i, (means, database_name) in enumerate(zip(means_list, database_names)):
-        plt.bar(x + i * width, means, width=width, label=database_name)
+        bar = plt.bar(x + i * width, list(map(lambda x : x / 1000, means)), width=width, label=database_name)
+        plt.bar_label(bar, fmt = '{:,.2f} s')
 
     plt.xlabel('Dataset Size')
-    plt.ylabel('Queries / Second')
-    plt.title(f'Average queries per second (for {nbr_query} Queries per query type)')
+    plt.ylabel('Execution Time (s)')
+    plt.title(
+        f'Average execution time (for {nbr_query} Queries per query type)')
     plt.xticks(x + width * (len(databases) - 1) / 2, dataset_size)
     plt.legend()
     plt.show()
